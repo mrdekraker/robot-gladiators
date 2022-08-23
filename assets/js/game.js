a96e9481fd5a35c4aea6c8b3b268a4e6a9cdcc3e
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-08-23 11:36:52 -0500

    initial js info. learned about functions and console.log()

---
+++ assets/js/game.js
@@ -0,0 +1,21 @@
+// *creats a function named "fight"
+
+function fight() {
+    window.alert(`The Fight Has Begun!`);
+}
+
+//  //fight();
+
+const playerName = window.prompt(`What is your robot's name?`);
+// Note the lack of quotation marks around playerName window.alert(playerName);
+
+console.log(playerName);
+
+console.log(`This logs a string, good for leaving yourself a message`);
+// *This will do math and log 20
+console.log(10 + 10);
+// *what is this?
+console.log(`Our robot's name is ${playerName}`);
+
+const name = `Mark`;
+console.log(name);
